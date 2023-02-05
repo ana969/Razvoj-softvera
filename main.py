a9154a48df9436b6fdd198beb796fe4173624176
ana969 <124468267+ana969@users.noreply.github.com>
2023-02-05 12:40:05 +0100

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,106 @@
+# def main():
+    print("Dobrodošli u kalkulator recikliranja!")
+    print("Ovaj program izračunava broj spašenih stabala i kornjača na temelju količine recikliranog papira i plastičnih boca.")
+    print("Počnimo spašavati planet zajedno!")
+
+    first_time = input("Jete li već prije koristili aplikaciju? (da/ne): ")
+    if first_time == "da":
+        mode = int(input("Unesite način rada (1-normalni, 2-mjesečni, 3-godišnji): "))
+        if mode == 1:
+            paper = int(input("Unesite broj kilograma recikliranog papira: "))
+            bottles = int(input("Unesite broj recikliranih plastičnih boca: "))
+            trees = paper / 10
+            turtles = bottles / 200
+            print("\nČestitamo! Ovo je utjecaj koji ste napravili:")
+            print(f"Spasili ste {trees:.2f} drveća i {turtles:.2f} kornjača")
+        elif mode == 2:
+            months = int(input("Unesite broj mjeseci: "))
+            total_trees = 0
+            total_turtles = 0
+            for i in range(months):
+                paper = int(input(f"Unesite broj kilograma recikliranog papira u mjesecu: {i+1}: "))
+                bottles = int(input(f"Unesite broj kilograma recikliranih plastičnih boca u mjesecu: {i+1}: "))
+                trees = paper / 10
+                turtles = bottles / 200
+                total_trees += trees
+                total_turtles += turtles
+            print("\nČestitamo! Ovo je utjecaj koji ste napravili:")
+            print(f"Spasili ste {trees:.2f} drveća i {turtles:.2f} kornjača u {months} mjeseca/mjeseci.")
+        elif mode == 3:
+            years = int(input("Unesite broj godina: "))
+            total_trees = 0
+            total_turtles = 0
+            for i in range(years):
+                paper = int(input(f"Unesite broj kilograma recikliranog papira u godini: {i+1}: "))
+                bottles = int(input(f"Unesite broj kilograma recikliranih plastičnih boca u godini: {i+1}: "))
+                trees = paper / 10
+                turtles = bottles / 200
+                total_trees += trees
+                total_turtles += turtles
+            print("\nČestitamo! Ovo je utjecaj koji ste napravili:")
+            print(f"Spasili ste {total_trees:.2f} drveća i {total_turtles:.2f} kornjača {years} godine/godina.")
+    else:
+        print("Uvijek je dobro početi! Ovako izgleda program: ")
+        paper = int(input("Unesite broj kilograma recikliranog papira:"))
+        bottles = int(input("Unesite broj recikliranih plastičnih boca:"))
+        trees = paper / 10
+        turtles = bottles / 200
+        print("\nČestitamo! Ovo je utjecaj koji ste napravili:")
+        print(f"Spasili ste {trees:.2f} drveća i  {turtles:.2f} kornjača. ")
+
+    print("\nNastavi s recikliranjem. Svako mlo djelo pomaže!")
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
+    play_game = input("Želiš li igrati kviz o recikliranju? (da/ne)")
+    
+    if play_game.lower() == 'da':
+        print("Dobrodošao u kviz!!")
+        print("U ovom kvizu ćeš odgovoriti na 3 kratka pitanja. ")
+        print("Pođimo provjeriti koliko znaš!\n")
+
+        questions = [
+            {
+                "question": "Koliko posto zauzima biootpad u komunalnom otpadu?", 
+                "options": ["oko 5%", "oko 20-30%", "više od pola"], 
+                "answer": 2
+            },
+            {
+                "question": "Gdje odlažemo namještaj?", 
+                "options": ["u plavu kantu", "u poseban kontejner", "nigdje od navedenog"], 
+                "answer": 3
+            },
+            {
+                "question": "Što je najmanje prihvatljiv način ophođenja s otpadom?", 
+                "options": ["odlaganje otpada", "spaljivanje otpada", "ponovno korištenje"], 
+                "answer": 1
+            }
+        ]
+
+        score = 0
+        for question in questions:
+            print(f"\n{question['question']}")
+            for i, option in enumerate(question['options']):
+                print(f"{i+1}. {option}")
+            guess = int(input("\nUnesi broj točnog odgovora: "))
+            if guess == question['answer']:
+                print("Točno!")
+                score += 1
+            else:
+                print("Netočno.")
+
+        print(f"\nDobio si {score} od {len(questions)} točnih")
+        if score / len(questions) >= 0.75:
+            print("Bravo! Pravi si stručnjak!")
+        else:
+            print("Nasavi učiti, zajedno možemo napraviti svijet boljim mjestom za život!")
+    else:
+        print("U redu, dođi sljedeći put!")
+
+if __name__ == "__main__":
+    main()
+
+    print("Doviđenja! Posjetite: zgeko.com")
